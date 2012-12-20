5c1d5729b6b704a176731b61ce41dcea5466ced6
alquerci <alquerci@email.com>
2012-12-20 15:14:02 +0100

    Add User README.md and LICENCE

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,3 +1,21 @@
+/**       GNU GENERAL PUBLIC LICENSE, Version 3
+ * cpulimit - CPU usage limiter for windows (cygwin)
+ * Copyright (C) 2012 Alexandre Quercia <alquerci@email.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // stdafx.h : fichier Include pour les fichiers Include système standard,
 // ou les fichiers Include spécifiques aux projets qui sont utilisés fréquemment,
 // et sont rarement modifiés
