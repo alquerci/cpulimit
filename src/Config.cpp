b01bde24c233e1a605a27bac56773d58b5bf3ca2
alquerci <alquerci@email.com>
2012-12-21 06:31:31 +0100

    Add missing notice

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,3 +1,21 @@
+/**       GNU GENERAL PUBLIC LICENSE, Version 3
+ * cpulimit - CPU usage limiter for windows (cygwin)
+ * Copyright (C) 2012 Alexandre Quercia <alquerci@email.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "stdafx.h"
 // vendor
 #include "../vendor/ermshiperete/getopt/getoptLib/getopt.h"
@@ -29,6 +47,10 @@ Config::~Config(void)
 
 void Config::PrintUsage(FILE * stream)
 {
+    fprintf(stream, "cpulimit Copyright (C) 2012 Alexandre Quercia\n");
+    fprintf(stream, "This program comes with ABSOLUTELY NO WARRANTY. This is free software,\n");
+    fprintf(stream, "and you are welcome to redistribute it under certain conditions.\n");
+    fprintf(stream, "\n");
     fprintf(stream, "Usage: cpulimit [OPTIONS...] TARGET\n");
     fprintf(stream, "    OPTIONS\n");
     fprintf(stream, "       -l, --limit=N   percentage of cpu allowed from 1 to 100\n");
@@ -40,7 +62,8 @@ void Config::PrintUsage(FILE * stream)
     fprintf(stream, "    TARGET must be exactly one of these:\n");
     fprintf(stream, "       -p, --pid=N     pid of the process (implies -z)\n");
     fprintf(stream, "       -e, --exe=FILE  name of the executable program file or path name\n");
-    fprintf(stream, "\nReport bugs to <alexandrequercia@orange.fr>.\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "Report bugs to <alquerci@email.com>.\n");
 }
 
 int Config::GetError()
